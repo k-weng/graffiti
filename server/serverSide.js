73b6a11e6c18df2e16cd204362a81e44109417df
Christopher Wang <czw@mit.edu>
2016-01-13 18:45:07 -0500

    Added rudimentary messaging

--- server/serverSide.js
+++ server/serverSide.js
@@ -1,3 +1,8 @@
 if (Messages.find().count() == 0){
-	Messages.insert({name: "chris", text: "test"});
+	Messages.insert({name: "chris",
+	text: "test",
+	});
+}
+if (Groups.find().count() == 0){
+	Groups.insert({name: "Group Test"});
 }
