8087a4f69a57655922eba0e541d7a5e15e9267e0
Christopher Wang <czw@mit.edu>
2016-01-13 21:47:35 -0500

    Able to add comments, groups not finished

--- client/templates/messages/message_send.js
+++ client/templates/messages/message_send.js
@@ -2,13 +2,23 @@ Template.messageSend.events({
 	"submit .submit-message":function(e){
 		e.preventDefault();
 		var input = $("#message-input").val();
+
 		console.log(input);
 		console.log("submitted");
+
 		e.stopPropagation();
-		Messages.insert({
+
+		var message = {
 			userId:Meteor.userId(), 
 			text: input,
 			timestamp: Date.now()
+		};
+
+		Meteor.call("messageSend", message, function(err, result){
+			if(err){
+				return alert("there's an error");
+			}
+			// Router.go('groupPage',{_id: this._id});
 		});
 
 		$("#message-input").val("");
